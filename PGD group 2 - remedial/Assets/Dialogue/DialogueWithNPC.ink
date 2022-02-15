9c9d64b007d8479b094071a6b02864ade5ae3947
Siem15 <siem.wesseling@hva.nl>
2022-02-15 17:54:01 +0100

    Dialogue manager
    added dialogue manager +test dialogue, next part is adding dialogue panels and ui so I can visualize everything

---
+++ PGD group 2 - remedial/Assets/Dialogue/DialogueWithNPC.ink
@@ -0,0 +1,3 @@
+Hey there!
+This is a test!
+Here's another line!
---
+++ PGD group 2 - remedial/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Ink.Runtime;
+
+public class DialogueManager : MonoBehaviour
+{
+    [Header("Dialogue UI")]
+
+    [SerializeField] private GameObject dialoguePanel;
+    
+    [SerializeField] private TextMeshProUGUI dialogueText;
+
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset inkJSON;
+
+    private Story currentStory;
+
+    private bool dialogueIsPlaying;
+
+    private static DialogueManager instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("Found more than one dialogue manager in the scene");
+        }
+        instance = this;
+    }
+    
+    public static DialogueManager GetInstance()
+    {
+        return instance;
+    }
+
+    private void Start()
+    {
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!dialogueIsPlaying)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit raycastHit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out raycastHit, 100f))
+            {
+                if (raycastHit.transform != null)
+                {
+                    CurrentClickedGameObject(raycastHit.transform.gameObject);
+                }
+            }
+        }
+    }
+
+    public void CurrentClickedGameObject(GameObject gameObject)
+    {
+        if (gameObject.tag == "NPC")
+        {
+            ContinueStory();
+        }
+    }
+    public void EnterDialogueMode(TextAsset inkJSON)
+    {
+        currentStory = new Story(inkJSON.text);
+        dialogueIsPlaying = true;
+
+        ContinueStory();
+    }
+
+    private void ExitDialogueMode()
+    {
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+        dialogueText.text = "";
+    }
+
+    private void ContinueStory()
+    {
+        if (currentStory.canContinue)
+        {
+            dialogueText.text = currentStory.Continue();
+        }
+        else
+        {
+            ExitDialogueMode();
+        }
+    }
+}
