7f8e92ad3ce41ab4ceb373e75ecc7c28c7249f4f
Mgx98 <najee.alberto@gmail.com>
2022-02-15 21:12:03 +0100

    .

--- PGD group 2 - remedial/Assets/Scripts/CameraController.cs
+++ PGD group 2 - remedial/Assets/Scripts/CameraController.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] public float movementSpeed;
-    [SerializeField] public float movementTime;
+    public Transform cameraTransform;
 
+    public float movementSpeed;
+    public float movementTime;
     public Vector3 newPos;
-
+    
     public Quaternion newRotation;
     public float rotationAmount;
+
+    public Vector3 zoomAmount;
+    public Vector3 newZoom;
 
     // Start is called before the first frame update
     void Start()
     {
         newPos = transform.position;
         newRotation = transform.rotation;
+        newZoom = cameraTransform.localPosition;
         
     }
 
@@ -46,7 +51,7 @@ public class CameraController : MonoBehaviour
             newPos += (transform.right * movementSpeed);
         }
 
-//rotation
+//Rotation
           if(Input.GetKey(KeyCode.Q))
         {
             newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
@@ -56,9 +61,21 @@ public class CameraController : MonoBehaviour
             newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
         }
 
+//Zoom
+ if(Input.GetKey(KeyCode.Z))
+        {
+            newZoom += zoomAmount;
+        }
+
+if(Input.GetKey(KeyCode.X))
+        {
+            newZoom -= zoomAmount;
+        }
+
 //To get smooth movement
         transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * movementTime);
         transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime *movementTime);
     }
 }
 
